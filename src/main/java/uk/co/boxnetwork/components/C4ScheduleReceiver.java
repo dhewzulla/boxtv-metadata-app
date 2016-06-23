22f4978970c07e03a2533bf9476b2a53f19420a9
Dilshat Hewzulla <hewzulla@gmail.com>
2016-06-23 22:42:05 +0100

    working

--- src/main/java/uk/co/boxnetwork/components/C4ScheduleReceiver.java
+++ src/main/java/uk/co/boxnetwork/components/C4ScheduleReceiver.java
@@ -1,6 +1,8 @@
 package uk.co.boxnetwork.components;
 
 import org.dom4j.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import uk.co.boxnetwork.model.Asset;
 
 @Service
 public class C4ScheduleReceiver {
+	private static final Logger logger=LoggerFactory.getLogger(C4ScheduleReceiver.class);
 	@Autowired
 	private BoxMedataRepository boxMetadataRepository;
 	
@@ -20,7 +23,12 @@ public class C4ScheduleReceiver {
 	public void process(Document document){
 		C4Metadata c4metadata=c4SchedulerParser.parse(document);
 		for(Asset asset: c4metadata.getAssets()){
-			boxMetadataRepository.createAsset(asset);
+			if(boxMetadataRepository.getAssetById(asset.getId())==null){
+				boxMetadataRepository.createAsset(asset);
+			}
+			else{
+				logger.info("asset already exists");
+			}
 		}
 	}
 	
