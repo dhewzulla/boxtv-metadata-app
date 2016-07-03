e54b048b0a7b02b009acde1f2bbc96c29240c086
Dilshat Hewzulla <hewzulla@gmail.com>
2016-07-03 12:18:30 +0100

    import

--- src/main/java/uk/co/boxnetwork/mule/transformers/c4/ImportC4ScheduleTransformer.java
+++ src/main/java/uk/co/boxnetwork/mule/transformers/c4/ImportC4ScheduleTransformer.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.mule.api.MuleMessage;
 import org.mule.module.http.internal.ParameterMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import uk.co.boxnetwork.components.BCVideoService;
@@ -16,6 +18,7 @@ import uk.co.boxnetwork.mule.transformers.BoxRestTransformer;
 import uk.co.boxnetwork.mule.util.MuleRestUtil;
 
 public class ImportC4ScheduleTransformer extends BoxRestTransformer{
+	private static final Logger logger=LoggerFactory.getLogger(ImportC4ScheduleTransformer.class);
 	
 	@Autowired
 	ImportC4ScheduleService importC4ScheduleService;
@@ -25,6 +28,8 @@ public class ImportC4ScheduleTransformer extends BoxRestTransformer{
 		String payload=null;
 		try {
 			payload = message.getPayloadAsString();
+			logger.info("recived the import schedule request:"+payload);
+			
 			ObjectMapper objectMapper=new ObjectMapper();	    
 			ImportScheduleRequest request;
 			
