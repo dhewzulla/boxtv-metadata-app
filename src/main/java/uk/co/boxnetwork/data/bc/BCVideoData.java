5db800b0da7a5bddb7b8c7b75de1b1a5aaf1786b
Dilshat Hewzulla <hewzulla@gmail.com>
2016-10-09 23:58:29 +0100

    matrial in import

--- src/main/java/uk/co/boxnetwork/data/bc/BCVideoData.java
+++ src/main/java/uk/co/boxnetwork/data/bc/BCVideoData.java
@@ -200,6 +200,7 @@ public class BCVideoData {
 					  else{
 						  episode.setCtrPrg(materialId);
 					  }
+					  episode.setMaterialId(GenericUtilities.arrayToSeparatedString(materialParts,"/"));
 				}
 				
 			}
